02fe307126062824285c38d1726c06451ea1f935
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2024-12-23 12:50:06 +0530

    Update chats.dart

--- lib/chats.dart
+++ lib/chats.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 class ChatBubble extends StatelessWidget {
   final String message;
   final bool isMyMessage;
