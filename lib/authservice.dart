3bb1156cf82cd1637dccbfd263e0b0cff91316e4
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2025-01-04 17:09:29 +0530

    Update authservice.dart

--- lib/authservice.dart
+++ lib/authservice.dart
@@ -16,7 +16,6 @@ class AuthService extends ChangeNotifier {
       throw Exception(e.code);
     }
   }
-
   Future<UserCredential> signUpWithEmailandPassword(
       String email, password) async {
     try {
