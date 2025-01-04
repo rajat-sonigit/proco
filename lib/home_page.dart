f87a470415e2e74ebbd0e0a531a39b63e13c7a53
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2025-01-04 17:09:55 +0530

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,7 +16,6 @@ class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
-
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
 
