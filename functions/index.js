c784d4033576f594468693d211439e01c2245f8d
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2025-04-23 09:07:53 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,6 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
